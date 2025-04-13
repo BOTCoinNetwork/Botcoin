01cf9cf3c92b7dac79cf8cce9a9fdab89e3900d7
stackmanxxx <stackmanxxx@gmail.com>
2025-04-13 16:39:26 +0800

    fix: config dir

--- src/configuration/const.go
+++ src/configuration/const.go
@@ -97,7 +97,7 @@ func DefaultMonetDir() string {
 		} else if runtime.GOOS == "windows" {
 			return filepath.Join(home, "AppData", "Roaming", "Monet")
 		} else {
-			return filepath.Join(home, ".monet")
+			return filepath.Join(home, ".botcoin")
 		}
 	}
 	return ""
